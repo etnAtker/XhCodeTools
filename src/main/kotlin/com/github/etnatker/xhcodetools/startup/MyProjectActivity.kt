d114fa325b2ee1d06ddb768bffd20ca1b0b26077
GitHub Action <action@github.com>
2025-04-27 08:04:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/etnatker/xhcodetools/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/etnatker/xhcodetools/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.etnatker.xhcodetools.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
